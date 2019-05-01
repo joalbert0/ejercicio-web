a01666a789475e6e56ba13b02d894a5760a20c5f
alberto <jorgealbertomancera@outlook.com>
2019-04-30 19:28:06 -0500

    v5

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/'));
-const http = require('http');
+
 app.listen('3000', function() {
     console.log('Servidor web escuchando en el puerto 3000');
 
 
 });
+
+function cargar1() {
+    document.getElementById("pty").style.display = "block";
+    document.getElementById("cuadrad").style.display = "block";
+    document.getElementById("new3").style.display = "none";
+    document.getElementById("new2").style.display = "none";
+    document.getElementById("btn1").style.backgroundColor = "rgb(33, 174, 199)";
+    document.getElementById("btn2").style.backgroundColor = "rgb(58, 151, 238)";
+    document.getElementById("btn3").style.backgroundColor = "rgb(58, 151, 238)";
+    $("pty").remove;
+};
+
+function cargar2() {
+    document.getElementById("cuadrad").style.display = "none";
+    document.getElementById("new3").style.display = "inline-grid";
+    document.getElementById("new2").style.display = "none";
+    document.getElementById("btn2").style.backgroundColor = "rgb(33, 174, 199)";
+    document.getElementById("btn1").style.backgroundColor = "rgb(58, 151, 238)";
+    document.getElementById("btn3").style.backgroundColor = "rgb(58, 151, 238)";
+
+
+}
+
+function cargar3() {
+    document.getElementById("cuadrad").style.display = "none";
+    document.getElementById("new3").style.display = "none";
+    document.getElementById("new2").style.display = "grid";
+    document.getElementById("btn3").style.backgroundColor = "rgb(33, 174, 199)";
+    document.getElementById("btn1").style.backgroundColor = "rgb(58, 151, 238)";
+    document.getElementById("btn2").style.backgroundColor = "rgb(58, 151, 238)";
+}
